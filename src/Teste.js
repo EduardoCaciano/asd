4955fd663376b689acdc4b7ed8d30df5c06dbc02
EDUARDO DA SILVA CACIANO <20272669@cfp127aluno.local>
2021-09-08 11:29:09 -0300

    atualização 08/09/2021

---
+++ src/Teste.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Teste() {
+
+    const [contador, setContador] = useState(0);
+
+    const [nomes, setNomes] = useState(["Eduardo", "Helena", "Matheus", "Jhonatan", "Kaik", "Julia", "Vanessa"]);
+
+    const [nome, setNome] = useState("");
+
+    const handleSend = () => {
+        setNomes([...nomes, nome]);
+
+        setNome("");
+    }
+
+    return (
+        <>
+            <h1>{contador}</h1>
+
+            <ul>
+                {nomes.map(nome => {
+                    return <li>{nome}</li>
+                })}
+            </ul>
+
+            <button onClick={() => setContador(contador + 1)}>Contar</button>
+
+            <br />
+            <br />
+            <br />
+
+            <input type="text" onChange={(e) => setNome(e.target.value)}
+                value={nome} placeholder="Digite seu nome" />
+
+            <br />
+
+            <button onClick={() => setNome("")}>Limpar</button>
+            <button onClick={handleSend}>Enviar</button>
+
+            <h2>{nome}</h2>
+        </>
+    )
+}
+
+export default Teste;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,41 @@
 import { Container, FormContainer } from "./styles";
 import Input from "../../components/Input";
+import { useState } from "react";
+import { api } from "../../services/api";
 
-function Login (){
+function Login() {
+
+    const [formLogin, setFormLogin] = useState({
+        email: "",
+        senha: ""
+    });
+
+    // handle input genérico
+    const handleInput = (e) => {
+        setFormLogin({ ...formLogin, [e.target.id]: e.target.value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post("/sessions", {
+                email: formLogin.email,
+                password: formLogin.senha
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
 
     return (
         <Container>
-            <FormContainer>
+            <FormContainer onSubmit={handleSubmit}>
                 <h1>Bem-Vindo ao</h1>
                 <h1>SENAI-Overflow</h1>
-                <Input label="E-mail" id="email" />
-                <Input label="Senha" id="senha" />
+                <Input label="E-mail" id="email" handler={handleInput} required/>
+                <Input label="Senha" id="senha" handler={handleInput} required/>
                 <button>Entrar</button>
             </FormContainer>
         </Container>
